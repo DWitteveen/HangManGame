12f3cabdc60a42bf030316ca0f3d61e64bf72e1c
DWitteveen <d.e.j.witteveen@gmail.com>
2018-04-07 19:23:16 +0200

    cleanup

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import './hangman.css';
+import Words from './words';
+import _ from 'underscore';
+import Letters from './letters'
 
 class App extends Component {
  render() {
@@ -9,6 +13,36 @@ class App extends Component {
      </div>
    )
  }
+
+ newGame() {
+   let guesses = [];
+   let strikes = 0;
+   let gameover = false;
+   let winning = false;
+   let word = _.sample(Words);
+   this.setState({guesses, strikes, gameover, winning, word});
+ }
+
+ hasWon() {
+   let {word, guesses} = this.state;
+   return !_.chain(word.split(''))
+   .map(letter => _.contains(guesses, letter))
+   .contains(false)
+   .value();
+ }
+
+
+
+ checkWinner() {
+   if (this.state.winning) {
+     return 'We have a winner'
+   } else if (this.state.gameover) {
+     return 'Loser'
+   } else {
+     return 'HangMan'
+   }
+ }
+
 }
 
 export default App;
